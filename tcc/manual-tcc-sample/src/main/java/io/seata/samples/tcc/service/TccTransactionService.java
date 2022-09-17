ff888e749420bb1c2fe9a1c043f51c9c2b3ad648
ruishansun <ruishansun@creditease.cn>
2022-09-17 18:59:20 +0800

    manual transaction draft version

---
+++ tcc/manual-tcc-sample/src/main/java/io/seata/samples/tcc/service/TccTransactionService.java
@@ -0,0 +1,79 @@
+/*
+ *  Copyright 1999-2021 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.samples.tcc.service;
+
+import io.seata.core.context.RootContext;
+import io.seata.core.exception.TransactionException;
+import io.seata.samples.tcc.action.TccActionTwo;
+import io.seata.samples.tcc.action.impl.TccActionOneImpl;
+import io.seata.tm.api.DefaultSeataClient;
+import io.seata.tm.api.GlobalTransaction;
+import io.seata.tm.api.GlobalTransactionContext;
+import io.seata.tm.api.SeataClient;
+import io.seata.tm.api.business.ManualTccActionOne;
+
+/**
+ * The type Tcc transaction service.
+ *
+ * @author zhangsen
+ */
+public class TccTransactionService {
+    
+
+    private TccActionTwo tccActionTwo;
+
+    /**
+     * 发起分布式事务
+     *
+     * @return string string
+     */
+
+    public String doTransactionCommit() throws TransactionException, InstantiationException, IllegalAccessException {
+        
+        GlobalTransaction tx = GlobalTransactionContext.getCurrentOrCreate();
+        try {
+            tx.begin(60000, "testBiz");
+
+            SeataClient seataClient = new DefaultSeataClient();
+
+            ManualTccActionOne tccActionOne = new TccActionOneImpl();
+            ManualTccActionOne seataClientProxy  = seataClient.createProxy(tccActionOne);
+            boolean result1 = seataClientProxy.prepare(null, 1);
+            
+            
+            //if data negative rollback else commit
+            if (result1) {
+                tx.commit();
+            } else {
+                tx.rollback();
+            }
+        } catch (Exception exx) {
+            tx.rollback();
+            throw exx;
+        }
+        return RootContext.getXID();
+    }
+
+
+    /**
+     * Sets tcc action two.
+     *
+     * @param tccActionTwo the tcc action two
+     */
+    public void setTccActionTwo(TccActionTwo tccActionTwo) {
+        this.tccActionTwo = tccActionTwo;
+    }
+}
---
+++ tcc/manual-tcc-sample/src/main/java/io/seata/samples/tcc/starter/ManualTccTransactionStarter.java
@@ -0,0 +1,92 @@
+/*
+ *  Copyright 1999-2021 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.samples.tcc.starter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.seata.common.util.StringUtils;
+import io.seata.core.exception.TransactionException;
+import io.seata.samples.tcc.ApplicationKeeper;
+import io.seata.samples.tcc.action.ResultHolder;
+import io.seata.samples.tcc.action.impl.TccActionOneImpl;
+import io.seata.samples.tcc.action.impl.TccActionTwoImpl;
+import io.seata.samples.tcc.service.TccTransactionService;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Assert;
+
+/**
+ * The type Local tcc transaction starter.
+ *
+ * @author zhangsen
+ */
+public class ManualTccTransactionStarter {
+
+    /**
+     * The Application context.
+     */
+    static AbstractApplicationContext applicationContext = null;
+
+    /**
+     * The Tcc transaction service.
+     */
+    static TccTransactionService tccTransactionService = null;
+
+    /**
+     * The Tcc action one.
+     */
+    static TccActionOneImpl tccActionOne = null;
+
+    /**
+     * The Tcc action two.
+     */
+    static TccActionTwoImpl tccActionTwo = null;
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws InterruptedException the interrupted exception
+     */
+    public static void main(String[] args) throws InterruptedException, TransactionException, InstantiationException, IllegalAccessException {
+        applicationContext = new ClassPathXmlApplicationContext(new String[] {"spring/seata-tcc.xml"});
+
+        tccTransactionService = (TccTransactionService)applicationContext.getBean("tccTransactionService");
+
+        tccActionOne = (TccActionOneImpl)applicationContext.getBean("tccActionOne");
+        tccActionTwo = (TccActionTwoImpl)applicationContext.getBean("tccActionTwo");
+
+        //分布式事务提交demo
+        transactionCommitDemo();
+        
+        new ApplicationKeeper(applicationContext).keep();
+    }
+
+    private static void transactionCommitDemo() throws InterruptedException, TransactionException, InstantiationException, IllegalAccessException {
+        String txId = tccTransactionService.doTransactionCommit();
+        System.out.println(txId);
+        Assert.isTrue(StringUtils.isNotBlank(txId), "事务开启失败");
+
+        Thread.sleep(1000L);
+
+        Assert.isTrue("T".equals(ResultHolder.getActionOneResult(txId)), "tccActionOne commit failed");
+        Assert.isTrue("T".equals(ResultHolder.getActionTwoResult(txId)), "tccActionTwo commit failed");
+
+        System.out.println("transaction commit demo finish.");
+    }
+
+}
